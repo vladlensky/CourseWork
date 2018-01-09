129d28eafd59f3ba9c72369fb5458320bedf41c0
Ваше Имя <you@example.com>
2018-01-10 00:57:42 +0300

    more canvas

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -7,12 +7,30 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
   @ViewChild('canvas') canvas: ElementRef;
+  @ViewChild('med') med: ElementRef;
+  @ViewChild('assault') assault: ElementRef;
+  @ViewChild('sniper') sniper: ElementRef;
+  @ViewChild('eng') eng: ElementRef;
   beginNumber = 4123;
   endNumber = 12567;
+  medPoints = 5434;
+  engPoints = 8643;
+  assaultPoints = 78534;
+  sniperPoints = 31233;
+  maxEP = 80000;
+  maxMP = 70000;
+  maxAP = 100000;
+  maxSP = 70000;
+  wins = 738;
+  loses = 1052;
+  deaths = 3589;
+  kills = 4658;
+  wlRatio = Math.round(this.wins / this.loses * 100) / 100;
+  kdRatio = Math.round(this.kills / this.deaths * 100) / 100;
   constructor() { }
 
   ngOnInit() {
-    const context = this.canvas.nativeElement.getContext('2d');
+    let context = this.canvas.nativeElement.getContext('2d');
     const centerX = this.canvas.nativeElement.width / 2;
     const centerY = this.canvas.nativeElement.height / 2;
     const radius = 69;
@@ -31,6 +49,38 @@ export class MainPageComponent implements OnInit {
     context.lineWidth = 3;
     context.font = 'italic 15pt Arial';
     context.fillText('' + this.beginNumber + ' / ' + this.endNumber, centerX - radius + 10, centerY + 5, 120);
+    //
+    context = this.med.nativeElement.getContext('2d');
+    context.beginPath();
+    context.moveTo(0, 50);
+    context.lineWidth = 202;
+    context.strokeStyle = 'blue';
+    context.lineTo(this.medPoints / this.maxMP * this.med.nativeElement.width, 50);
+    context.stroke();
+    //
+    context = this.assault.nativeElement.getContext('2d');
+    context.beginPath();
+    context.moveTo(0, 50);
+    context.lineWidth = 202;
+    context.strokeStyle = 'green';
+    context.lineTo(this.assaultPoints / this.maxAP * this.assault.nativeElement.width, 50);
+    context.stroke();
+    //
+    context = this.sniper.nativeElement.getContext('2d');
+    context.beginPath();
+    context.moveTo(0, 50);
+    context.lineWidth = 202;
+    context.strokeStyle = 'red';
+    context.lineTo(this.sniperPoints / this.maxSP * this.sniper.nativeElement.width, 50);
+    context.stroke();
+    //
+    context = this.eng.nativeElement.getContext('2d');
+    context.beginPath();
+    context.moveTo(0, 50);
+    context.lineWidth = 202;
+    context.strokeStyle = 'yellow';
+    context.lineTo(this.engPoints / this.maxEP * this.eng.nativeElement.width, 50);
+    context.stroke();
   }
 
 }
